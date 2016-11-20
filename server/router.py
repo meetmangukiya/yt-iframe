f0f11768da46c1fb2f7a6ee8a71921a0f43691fa
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-11-20 12:19:56 +0530

    router: Set port to 2000

--- server/router.py
+++ server/router.py
@@ -1,4 +1,4 @@
-# The name `main.py` will also probably change, it is not permanent! 
+# The name `main.py` will also probably change, it is not permanent!
 # TMP
 
 from flask import Flask
@@ -11,11 +11,11 @@ app = Flask(__name__)
 def root():     # TMP
     return "You seem to have been stumbled upon a website that is probably not for you" \
             "unless you were invited too! Anyways, thanks for passing by!"
-            
+
 @app.route('/play')
 def play():
     ids = request.args.getlist('id')
     return render_template('index.html', ids=ids)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=2000)
