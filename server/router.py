51f26fb70da4a02b3a33e071831ae4ac7bd65f4c
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-11-20 12:22:16 +0530

    router: Log the contents of ids variable

--- server/router.py
+++ server/router.py
@@ -1,6 +1,3 @@
-# The name `main.py` will also probably change, it is not permanent!
-# TMP
-
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -11,13 +8,14 @@ def log(string):
     print("LOG:", string)
 
 @app.route('/')
-def root():     # TMP
+def root():
     return "You seem to have been stumbled upon a website that is probably not for you" \
             "unless you were invited too! Anyways, thanks for passing by!"
 
 @app.route('/play')
 def play():
     ids = request.args.getlist('id')
+    log("ids:", ids)
     return render_template('player.html', ids=ids)
 
 if __name__ == "__main__":
